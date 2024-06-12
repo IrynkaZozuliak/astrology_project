8cb7ff6f318f0d2c32c4b7c2bb41cafcdad9aea4
IrynkaZozuliak <irynka.zozuliak@gmail.com>
2024-06-12 18:13:35 +0000

    fghjkkk

--- javascript/testimonials.js
+++ javascript/testimonials.js
@@ -69,3 +69,13 @@ document.addEventListener("mouseup", dragStop);
 carousel.addEventListener("scroll", infiniteScroll);
 wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
 wrapper.addEventListener("mouseleave", autoPlay);
+
+
+
+
+
+
+
+
+
+
